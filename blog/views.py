f4358878db49bc23ea0342921e5a05ca7f4afdb3
davicosta66 <davi.costa.66@outlook.com>
2019-11-13 15:51:41 -0300

    Mudança na views

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,4 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts=Post.objects.filter(published_date_lte=timezone.now()).oreder_by('published_date')
-    return render(request,'blog/post_list.html',{'posts':posts})
+    return render(request,'blog/post_list.html',{})
